debeae34a0594ab2cc6174497029eaf406e3f770
Jessica Peng <jessica.peng@nytimes.com>
2018-07-23 12:47:16 -0400

    update

--- js/camera.js
+++ js/camera.js
@@ -1,5 +1,7 @@
 <script src = "three.js-master/src/THREE.js"></script>
+<script src = "three.js-master/build/three.js"></script>
 <script src = "three.js-master/src/cameras/PerspectiveCamera.js"></script>
+
 var camera, controls, scene, renderer;
 
 init();
@@ -7,6 +9,7 @@ init();
 animate();
 
 function init() {
+
   camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.set(0, 0, 0);
 
@@ -14,7 +17,6 @@ function init() {
   controls = new THREE.OrbitControls(camera, renderer.domElement);
 
   //controls.addEventListener('change', render); //call only in static scenes
-
   controls.enableDamping = true;
   controls.dampingFactor = 0.25;
 
@@ -24,4 +26,23 @@ function init() {
   controls.maxDistance = 500;
 
   controls.maxPolarAngle = Math.PI / 2;
+
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
+}
+
+function render(){
+  renderer.render(scene, camera);
 }
