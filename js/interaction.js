8683c527b8b29d58261db78b3394c87a821761aa
Le <chloe.le@nytimes.com>
2018-07-24 18:08:06 -0400

    only allowing alphabets

--- js/interaction.js
+++ js/interaction.js
@@ -31,50 +31,51 @@ function onDocumentKeyDown( event ) {
   //console.log("This was the key that was pressed: "+keyCode);
   //TODO: On Key Pressed Logic
   if ( INTERSECTED ) {
+      if ((keyCode >= 65 && keyCode <= 120) || (keyCode == 32 && keyCode == 0)) {
+          var x = document.createElement("canvas");
+          var xc = x.getContext("2d");
+          x.width = x.height = 128;
+          xc.fillStyle = "white";
+          xc.fillRect(0, 0, 128, 128);
+          xc.shadowColor = "#000";
+          xc.shadowBlur = 7;
+          xc.fillStyle = "black";
+          xc.font = "64pt arial bold";
+          xc.textAlign = "center";
+          xc.fillText(String.fromCharCode(keyCode), 64, 96);
 
-    var x = document.createElement("canvas");
-    var xc = x.getContext("2d");
-    x.width = x.height = 128;
-    xc.fillStyle = "white";
-    xc.fillRect(0, 0, 128, 128);
-    xc.shadowColor = "#000";
-    xc.shadowBlur = 7;
-    xc.fillStyle = "black";
-    xc.font = "64pt arial bold";
-    xc.textAlign = "center";
-    xc.fillText(String.fromCharCode(keyCode), 64, 96);
+          var cmap = new THREE.Texture(x);
+          INTERSECTED.material.map = cmap;
+          INTERSECTED.material.map.needsUpdate = true;
 
-    var cmap = new THREE.Texture(x);
-    INTERSECTED.material.map = cmap;
-    INTERSECTED.material.map.needsUpdate = true;
+          // click -> set global state variable to 0, click again and increment variable..pass variable into object name argument
+          var clicked = INTERSECTED;
+          var newPosition = INTERSECTED.position.clone();
+          if (!window.hasOwnProperty("directionIndex")) {
+              directionIndex = 0
+          }
+          var dir = ["x", "y", "z"][directionIndex];
+          var keys = Object.keys(INTERSECTED.words);
+          if (!keys.includes(dir)) {
+              directionIndex = (directionIndex + 1) % 3;
+              onDocumentKeyDown(event);
+          }
+          if (dir === 'y') {
+              newPosition[dir] -= 1;
+          } else {
+              newPosition[dir] += 1;
+          }
 
-    // click -> set global state variable to 0, click again and increment variable..pass variable into object name argument
-    var clicked = INTERSECTED;
-    var newPosition = INTERSECTED.position.clone();
-    if (!window.hasOwnProperty("directionIndex")) {
-      directionIndex = 0
-    }
-    var dir = ["x", "y", "z"][directionIndex];
-    var keys = Object.keys(INTERSECTED.words);
-    if (!keys.includes(dir)) {
-      directionIndex = (directionIndex + 1) % 3;
-      onDocumentKeyDown(event);
-    }
-    if (dir === 'y') {
-      newPosition[dir] -= 1;
-    } else {
-      newPosition[dir] += 1;
-    }
+          console.log("Old Position: ", INTERSECTED);
+          var nextBlock = scene.getObjectByName(newPosition.x + "-" + newPosition.y + "-" + newPosition.z);
+          if (nextBlock) {
+              INTERSECTED.material.color.setHex(0xFFFFFF);
+              INTERSECTED = scene.getObjectByName(newPosition.x + "-" + newPosition.y + "-" + newPosition.z);
+              INTERSECTED.material.color.setHex(0xFCD931);
+          }
 
-    console.log("Old Position: ",INTERSECTED);
-    var nextBlock = scene.getObjectByName(newPosition.x+"-"+newPosition.y+"-"+newPosition.z);
-    if (nextBlock) {
-      INTERSECTED.material.color.setHex( 0xFFFFFF );
-      INTERSECTED = scene.getObjectByName(newPosition.x+"-"+newPosition.y+"-"+newPosition.z);
-      INTERSECTED.material.color.setHex( 0xFCD931 );
-    }
+          console.log("New intersects: ", INTERSECTED);
 
-    console.log("New intersects: ",INTERSECTED);
-
+      }
   }
 }
