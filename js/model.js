f63da87657518efbe43b9754df6b6605876a5aca
John Westwig <john.westwig@nytimes.com>
2018-07-25 11:27:58 -0400

    Backspace

--- js/model.js
+++ js/model.js
@@ -49,7 +49,7 @@ loadJSON(function(response) {
         xc.fillStyle = "black";
         xc.font = "64pt arial bold";
         xc.textAlign = "center";
-        xc.fillText(letter, 64, 96);
+        //xc.fillText(letter, 64, 96);
 
         var xm = new THREE.MeshBasicMaterial({
             map: new THREE.Texture(x),
@@ -104,8 +104,6 @@ loadJSON(function(response) {
         }
         return puzzleGroup;
     }
-    
+
     const puzzleGroup = drawCrossword(scene);
 });
-
-
